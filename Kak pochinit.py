b31f709d1d5fafd36b3a2f2b0db6b9f831c1a4e8
Olga <olga.korop@sweetstackdigital.com>
2023-06-25 17:27:36 +0200

    Var

--- Kak pochinit.py
+++ Kak pochinit.py
@@ -2,26 +2,37 @@
 
 def fix_it():
     print("Это двигается?")
-    answer1 = input("Да или нет? ")
-    if answer1 == "да":
-        print("А должно?")
-        answer2 = input("Да или нет? ")
-        if answer2 == "да":
-            print("Значит не трогай")
-        else:
-            print("Значит намотай изоленту")
-    elif answer1 == "нет":
-        print("А должно?")
-        answer2 = input("Да или нет? ")
-        if answer2 == "нет":
-            print("Значит не трогай")
-        else:
-            print("Значит пшикни WD-40")
-
-
-
+    answer1 = input("да или нет? ").lower()
+    if answer1 == "да" or answer1 == "нет":
+        if answer1 == "да":
+            print("А должно?")
+            answer2 = input("да или нет? ").lower()
+            if answer2 == "да" or answer2 == "нет":
+                if answer2 == "да":
+                    print("Значит не трогай")
+                else:
+                    print("Значит намотай изоленту")
+            else:
+                print("Введите корректные данные")
+        elif answer1 == "нет":
+            print("А должно?")
+            answer2 = input("да или нет? ").lower()
+            if answer2 == "да" or answer2 == "нет":
+                if answer2 == "нет":
+                    print("Значит не трогай")
+                else:
+                    print("Значит пшикни WD-40")
+            else:
+                print("Введите корректные данные")
     else:
-        print("А должно?")
+        print("Введите корректные данные")
+
+
+
+
+
+
+
 
 # Вызываем функцию для выполнения задачи
 fix_it()
