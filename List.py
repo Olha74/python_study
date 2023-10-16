4b898e9c822d6b4f4648a3393826ec93236dd044
Olga <olga.korop@sweetstackdigital.com>
2023-10-16 23:03:23 +0200

    Var

---
+++ List.py
@@ -0,0 +1,49 @@
+# a = [3, 5, 20]
+# print(a)
+# a.append(18)
+# print(a)
+# a.append("Olha")
+# print(a)
+# a.append([3,8])
+# print(a)
+# a.pop()
+# print(a)
+# print(a[-1])
+# a[0] = 100
+# print(a)
+# a.pop()
+# print(a)
+# a.pop(1)
+# print(a)
+#
+
+# list_of_cities = ['USA', ['Europe', ['Ukraine', 'Germany', ['Berlin', 'Munich', 'Mannheim'], 'France']]]
+# print(list_of_cities[1][1][2][2])
+
+# list_dlina = [1, 11, 43, 6, 333, 79, 42, 4, 3, 789, 97]
+# print(len(list_dlina))
+
+test_list = ['Завтрак', 'Ужин', 'Обед',]
+if 'Понедельник'in test_list:
+    print('Понедельник')
+elif 'Вторник' in test_list:
+    print('Вторник')
+elif 'Среда' in test_list:
+    print('Среда')
+elif 'Четверг'in test_list:
+    print('Четверг')
+elif 'Пятница' in test_list:
+    print('Пятница')
+elif 'Суббота' in test_list:
+    print('Суббота')
+elif 'Воскресенье' in test_list:
+    print('Воскресенье')
+else:
+    print("Ужин")
+
+
+
+
+
+
+
